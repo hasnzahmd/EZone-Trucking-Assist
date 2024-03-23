b9fe5d4e2f906c5e335a3b97b5242b686e626790
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-03-23 20:30:17 +0500

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
     <button
       onClick={props.onClick}
       className={`bg-[#e87726] text-white text-sm font-medium py-[0.8rem] px-4 rounded ${props.width ? '': 'sm:w-max'} ${props.borderWhite && 'border border-white'} transform 
-      focus:outline-none focus:shadow-outline transition duration-150 ease-in-out active:scale-90 uppercase`}
+      focus:outline-none transition duration-150 ease-in-out active:scale-90 uppercase`}
     >
       {props.text}
     </button>
